ced3b0182750aa60776a9ec069450d8560829810
Tarun Nagabramhagna <tharunnagabramhagna@gmail.com>
2026-04-20 17:13:24 +0530

    "Updated files"

--- DSA Concepts/16-Graphs.cpp
+++ DSA Concepts/16-Graphs.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <vector>
 #include <list>
+#include <queue>
 using namespace std;
 
 class Graph
@@ -30,6 +30,58 @@ public:
             cout << endl;
         }
     }
+
+    // BFS traversal //
+    void BFS(int st)
+    {
+        queue<int> q;
+        vector<bool> vis(V, false);
+
+        q.push(st); // push source vertex first
+        vis[0] = true;
+
+        while (q.size() > 0)
+        {
+            int u = q.front(); // u => source vertex
+            q.pop();
+            cout << u << " ";
+            for (int v : l[u])
+            { // v => destination vertex
+                if (!vis[v])
+                {
+                    vis[v] = true;
+                    q.push(v);
+                }
+            }
+        }
+
+        cout << endl;
+    }
+
+    // DFS traversal //
+    void helper(int u, vector<bool> &vis)
+    {
+        cout << u << " ";
+        vis[u] = true;
+        for (int v : l[u])
+        {
+            if (!vis[v])
+                helper(v, vis);
+        }
+    }
+
+    void DFS()
+    {
+        int src = 0;
+        vector<bool> vis(V, false);
+        helper(src, vis);
+
+        // for disconnected graphs
+        // for(int i = 0; i < V; i++) {
+        //     if(!vis[i])
+        //         helper(i,vis);
+        // }
+    }
 };
 
 int main()
@@ -41,7 +93,21 @@ int main()
     g.addEdge(1, 3);
     g.addEdge(2, 3);
     g.addEdge(2, 4);
+    cout << "The Adjacency List : \n";
     g.printAdjList();
+
+    Graph g1(5);
+    g1.addEdge(0, 1);
+    g1.addEdge(1, 2);
+    g1.addEdge(1, 3);
+    g1.addEdge(2, 4);
+    cout << "\nBFS traversal : ";
+    int source = 0;
+    g1.BFS(source);
+    cout << endl;
+    cout << "DFS traversal : ";
+    g1.DFS();
+    cout << endl;
 
     return 0;
 }
