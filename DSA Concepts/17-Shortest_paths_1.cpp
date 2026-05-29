9de6fddc46fbcb32d4557aeef9fbf31ca10d8404
Tarun Nagabramhagna <tharunnagabramhagna@gmail.com>
2026-05-29 15:21:21 +0530

    Updated files

---
+++ DSA Concepts/17-Shortest_paths_1.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <list>
+#include <vector>
+#include <queue>
+#include <climits>
+using namespace std;
+
+class Edge
+{
+public:
+    int v;
+    int wt;
+    Edge(int v, int wt)
+    {
+        this->v = v;
+        this->wt = wt;
+    }
+};
+
+void DijkstraAlgo(int src, vector<vector<Edge>> g, int V)
+{
+    vector<int> dist(V, INT_MAX); // INT_MAX represents infinity
+    dist[src] = 0;                // set source distance to 0
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    // 'greater' and 'vector' are used to create a min-heap priority queue
+    // pair: (dist[u], u)
+
+    pq.push({0, src}); // push the source vertex first
+
+    while (pq.size() > 0)
+    {
+        int u = pq.top().second;
+        pq.pop();
+        for (Edge e : g[u])
+        { // edge relaxation
+            if (dist[e.v] > dist[u] + e.wt)
+                dist[e.v] = dist[u] + e.wt;
+            pq.push({dist[e.v], e.v}); // push neighbors
+        }
+    }
+
+    // Print the shortest paths
+    cout << "Shortest paths from the source vertex:\n";
+    for (int i = 0; i < V; i++)
+        cout << dist[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int V = 6;
+    vector<vector<Edge>> g(V);
+
+    g[0].push_back(Edge(1, 2));
+    g[0].push_back(Edge(2, 4));
+
+    g[1].push_back(Edge(2, 1));
+    g[1].push_back(Edge(3, 7));
+
+    g[2].push_back(Edge(4, 3));
+
+    g[3].push_back(Edge(5, 1));
+
+    g[4].push_back(Edge(3, 2));
+    g[4].push_back(Edge(5, 5));
+
+    cout << "\n--Dijkstra's Algorithm--\n";
+    DijkstraAlgo(0, g, V);
+
+    return 0;
+}
